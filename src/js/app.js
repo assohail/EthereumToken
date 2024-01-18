0513bf87aa8980d03ac4b8b579fe8a62183dd64f
assohail <aamirsohail9210@gmail.com>
2024-01-18 19:45:18 +0500

    login logout functionality

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ App = {
   contracts: {},
   // account: '0x0',
   account:'0x0000000000000000000000000000000000000000',
+  account_balance: 0,
+  sign: '',
   loading: false,
   tokenPrice: 1000000000000000,
   tokensSold: 250000,
@@ -18,7 +20,7 @@ App = {
     
     if(typeof web3 !== 'undefined') {
       //check Metamask lock
-      App.isMetamaskLocked();
+      // App.isMetamaskLocked();
       // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       web3  = new Web3(web3.currentProvider);
@@ -27,74 +29,32 @@ App = {
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
     }
-    return App.initContracts();
+    $('#exampleModal').modal('show');
+    // return App.initContracts();
   },
 
-    isMetamaskLocked: async function () {
-    // await window.ethereum.enable(); // No need to enable this if MetaMask there
-    // Other check for MetaMask after checking of web3 availability
-    if(window.ethereum.isMetaMask){
+  isMetamaskLocked: async function () {
+  // await window.ethereum.enable(); // No need to enable this if MetaMask there
+  // Other check for MetaMask after checking of web3 availability
+  if(window.ethereum.isMetaMask){
       console.log('Meta Mask Detected!');
-      window.ethereum.request({ method: 'eth_requestAccounts' }).then(function(accounts){
+      await window.ethereum.request({ method: 'eth_requestAccounts' }).then(function(accounts){
         if(accounts.length !== 0){
           console.log('eth_Accounts:'+ accounts);
-          App.render();
+          App.account = accounts[0]
           console.log('Address added..')
         }else {
           console.log('Pls add Meta Mask account.');
         }
-      }).then(function(err){
-        if(err){
-          console.log('err :'+ err);
-        }
-        
-      });
-      
-      // If MetaMask is not locked
-      if(App.account === '0x0000000000000000000000000000000000000000'){
-        if (ethereum.selectedAddress){
-          console.log('Address:' + ethereum.selectedAddress);
-          App.account = ethereum.selectedAddress;
-          App.render();
-          console.log('Added address.');
-        } else {
-          console.log('Pls unlock your Meta Mask account.');
-        }
-      }
-
+      })
     } else {
       console.log('Pls install Meta Mask');
     }
-    // const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    // for(i=0; i<accounts.length; i++){
-    //  console.log(accounts.length+" iter:"+ i+1);
-    //  console.log(accounts);
-    // }
-    
-    //const accounts = ethereum.eth_requestAccounts();
-    //const account = accounts[0];
-    //console.log('Ac:'+account);
-
-
     
     // When user changes account, reload the App for the new account
     window.ethereum.on('accountsChanged', function (accounts) {
       App.render();
     });
-
-    //accounts(function(err, accounts){
-    //  if (err != null) {
-    //    console.log(err)
-    //  }else if (accounts.length === 0) {
-    //    console.log('MetaMask is locked')
-    //  }else {
-    //    console.log('MetaMask is unlocked')}
-    //  });
-
-    // console.log('before unlocked');
-    // const isUnlocked = await window?.ethereum?._metamask.isUnlocked();
-    // console.log('unlocked');
-    // console.debug({ isUnlocked });
 
 
     ethereum.on("chainChanged", (_chainId) => {
@@ -128,6 +88,99 @@ App = {
         });
     });
     // return App.render(); //GIVES PROMISE ERROR
+  },
+
+  btnhandler: async function() {
+    if (App.account === '0x0000000000000000000000000000000000000000') {
+      await App.isMetamaskLocked();
+      console.log('there')
+    }
+		// Asking if metamask is already present or not
+		if (window.ethereum) {
+			// res[0] for fetching a first wallet
+			await window.ethereum
+				.request({ method: "eth_requestAccounts" })
+				.then((res) => {
+					App.accountChangeHandler(res[0])
+        });
+		} else {
+			alert("install metamask extension!!");
+		}
+	},
+
+  	// Function for getting handling all events
+	accountChangeHandler: (account) => {
+		// Setting an address data
+		// setdata({
+		// 	address: account,
+		// });
+    App.account = account;
+
+		// Setting a balance
+		App.getbalance(account);
+	},
+
+  getbalance: (address) => {
+		// Requesting balance method
+		window.ethereum
+			.request({
+				method: "eth_getBalance",
+				params: [address, "latest"],
+			})
+			.then((balance) => {
+        console.log(balance)
+					App.account_balance = web3.fromWei(balance, 'ether')
+        console.log(App.account_balance)
+			});
+	},
+
+  /**
+ * Sign In With Ethereum helper
+ */
+  
+  siweSign: async (siweMessage) => {
+    try {
+      const encoder = new TextEncoder();
+      const data = encoder.encode(siweMessage);
+      
+      const from = App.account;
+      const msg = `0x${Array.prototype.map.call(data, x => ('00' + x.toString(16)).slice(-2)).join('')}`;
+      
+      const sign = await ethereum.request({
+        method: 'personal_sign',
+        params: [msg, from, 'Example password'],
+      });
+      App.sign = sign;
+      console.log(sign)
+    } catch (err) {
+      console.error(err);
+    }
+  },
+
+
+  /**
+   * Sign In With Ethereum
+   */
+  siwe: async () => {
+    const domain = window.location.host;
+    if (App.account === '0x0000000000000000000000000000000000000000') {
+      await App.isMetamaskLocked();
+    }
+    const from = App.account;
+    console.log('App.account');
+    console.log(App.account);
+    
+    const siweMessage = `${domain} wants you to sign in with your Ethereum account:\n${from}\n\nI accept the MetaMask Terms of Service: https://community.metamask.io/tos\n\nURI: https://${domain}\nVersion: 1\nChain ID: 1\nNonce: 32891757\nIssued At: 2021-09-30T16:25:24.000Z`;
+    await App.siweSign(siweMessage);
+    if (App.sign !== null){
+      $('#exampleModal').modal('hide');
+      $('#logout').css('display', 'block');
+      return App.initContracts();
+    }
+  },
+
+  logout: function() {
+    return window.location.reload();
   },
 
   buyTokens: function() {
@@ -198,6 +251,9 @@ App = {
         }
       }
     });
+    
+    // signature
+    $('#signature').html("Sign: " + App.sign);
 
     // Load token sale contract
     App.contracts.AMZTokenSale.deployed().then(function(instance){
